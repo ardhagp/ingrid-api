6ab9e3e0c033558d821bb386887f7f674955ba39
Ardha Gp <tito.ardha@windowslive.com>
2025-08-08 20:05:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
-<html>
-<head>
-<title>My First API Page</title>
-</head>
-<body><h1>Hi Vio!, thank you for your help</h1></body>
-</html>
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+$app->get('/ping', function (Request $request, Response $response) {
+    $data = [
+        'status' => 'ok',
+        'message' => 'Ingrid is alive and listening'
+    ];
+    $response->getBody()->write(json_encode($data));
+    return $response->withHeader('Content-Type', 'application/json');
+});
