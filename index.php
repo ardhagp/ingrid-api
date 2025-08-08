8c45fbbb08e355d6e8c9b041fd9c97f2579f22cd
Ardha Gp <tito.ardha@windowslive.com>
2025-08-08 20:10:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+
+$app = AppFactory::create();
 
 $app->get('/ping', function (Request $request, Response $response) {
     $data = [
@@ -9,3 +15,5 @@ $app->get('/ping', function (Request $request, Response $response) {
     $response->getBody()->write(json_encode($data));
     return $response->withHeader('Content-Type', 'application/json');
 });
+
+$app->run();
