03e394ad386acf1c6e51ac1d46bb25d6eea8b9ce
Ardha Gp <tito.ardha@windowslive.com>
2025-08-08 20:16:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,8 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
+<html>
+    <head><title></title>
+    </head>
 
-use Slim\Factory\AppFactory;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-
-$app = AppFactory::create();
-
-$app->get('/ping', function (Request $request, Response $response) {
-    $data = [
-        'status' => 'ok',
-        'message' => 'Ingrid is alive and listening'
-    ];
-    $response->getBody()->write(json_encode($data));
-    return $response->withHeader('Content-Type', 'application/json');
-});
-
-$app->run();
+    <body>
+        
+    </body>
+</html>
